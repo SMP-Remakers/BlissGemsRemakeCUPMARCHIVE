4959875521846ab2d9852334dffc85b6379459ca
ITzCupm <imrealmcraft2d@gmail.com>
2024-07-23 17:12:28 +0200

    Update Strength.java

--- src/main/java/com/itsundefined/blissgems/Strength.java
+++ src/main/java/com/itsundefined/blissgems/Strength.java
@@ -3,21 +3,15 @@ package com.itsundefined.blissgems;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-<<<<<<< HEAD
+
 public class Strength{
 
-    public static void StrengthInv(InventoryClickEvent event) {
-     Player p = event.getWhoClicked();
-     //Item
-
+    public static void StrengthInv(InventoryClickEvent InventoryClickPlayer) {
+     Inventory BlissGemsPlayer = InventoryClickPlayer.getClickedInventory();
+     ItemStack BlissGemItem =
+     InventoryClickPlayer.setCancelled(true);
     }
-    // bruh ok ill work on
-    //i gtg
-    //i will push changes
-=======
-// fuck u
->>>>>>> 662e3a63c0efdd33b415bdcb4ebb0173e7a21d35
-
 }
